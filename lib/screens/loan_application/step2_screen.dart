712014145a05a7a8a03a6e2754b6c6fdc71451d7
chalan-n <chalantornd@gmail.com>
2026-03-27 11:58:08 +0700

    fix: เพิ่ม import flutter/services.dart ใน step2_screen (UpperCaseTextFormatter)

--- lib/screens/loan_application/step2_screen.dart
+++ lib/screens/loan_application/step2_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
